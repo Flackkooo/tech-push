3936a91b7b0e44cd80db4e13192d94a197588c10
flackko <philipsraphael2@gmail.com>
2026-04-15 15:37:40 +0100

    new files to be committed

---
+++ error.js
@@ -0,0 +1,23 @@
+// try {
+//   let user = null;
+//   console.log(user.name);
+// } catch (err) {
+//   console.log(err.name);
+//   console.log(err.message);
+// } finally {
+//   console.log("Process finished.");
+// }
+
+// try {
+//   let balance = -10;
+//   if (balance < 0) {
+//     throw new Error("insufficient funds");
+//   }
+
+//   console.log("Transaction Successful");
+// } catch (err) {
+//   console.log(err.name);
+//   console.log(`An error occured: ${err.message}`);
+// } finally {
+//   console.log("Process Finished");
+// }
